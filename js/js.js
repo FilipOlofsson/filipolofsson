b26e2f0e9fae1a021066cce4bf005f7c8fe7ebf7
FilipOlofsson <te15.filip.olofsson@umea.nti.se>
2017-05-10 23:02:22 +0200

    Add files via upload

---
+++ js/js.js
@@ -0,0 +1,41 @@
+$(document).ready(function(){
+  $("a").click(function(e) {
+    e.preventDefault();
+    $($.attr(this, 'href')).addClass('active');
+    $('html, body').animate({
+      scrollTop: $($.attr(this, 'href')).offset().top
+    }, 1000);
+  });
+});
+$(document).ready(function() {
+  	$("a").click(function(e) {
+		setTimeout(function() {
+    		$(".active").removeClass('active');
+		}, 2000);
+  	});
+});
+$(document).keydown(function (e) {
+  switch(e.which) 
+  {
+       case 38:
+           	e.preventDefault();
+    		$('html, body').animate({ scrollTop: 0}, 1000);
+           break;
+       case 40:
+           $('html, body').animate({ scrollTop: $(window).height()}, 1000);
+           break;
+   }
+
+});
+$(window).bind('mousewheel', function(event) {
+    if (event.originalEvent.wheelDelta >= 0) {
+        $('html, body').animate({ scrollTop: 0}, 1000);
+    }
+    else {
+        $('html, body').animate({ scrollTop: $(window).height()}, 1000);
+    }
+});
+$('html, body').css({
+    overflow: 'hidden',
+    height: '100%'
+});
